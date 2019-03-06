9d2b9e72c228b0699407aeffd28109afe28c3854
bmcmahen <ben.mcmahen@gmail.com>
2019-03-06 14:16:19 -0800

    use camel case for prefixed styles

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -37,10 +37,11 @@ module.exports = {
         name: `Sancho`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#228be6`,
+        include_favicon: true,
+        theme_color: `#228be6`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/donkey.svg`, // This path is relative to the root of the site.
       },
     },
 
--- src/LoadingPlaceholder.tsx
+++
@@ -1,9 +0,0 @@
-/** @jsx jsx */
-import { jsx } from "@emotion/core";
-import * as React from "react";
-
-interface LoadingPlaceholderProps {}
-
-export function LoadingPlaceholder({  }: LoadingPlaceholderProps) {
-  return <div />;
-}
