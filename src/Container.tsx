a694e4c9c3fce1c110abf32b237ab82f53f869a4
bmcmahen <ben.mcmahen@gmail.com>
2019-03-21 00:08:55 -0700

    export responsive container padding for use outside of a container

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,8 +1,15 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { jsx, css } from "@emotion/core";
 import * as React from "react";
 import theme from "./Theme";
 import PropTypes from "prop-types";
+
+export const responsiveContainerPadding = css({
+  padding: `0 ${theme.spaces.md}`,
+  [theme.breakpoints.lg]: {
+    padding: `0 ${theme.spaces.lg}`
+  }
+});
 
 interface ContainerProps extends React.HTMLAttributes<HTMLElement> {}
 
@@ -11,16 +18,15 @@ export const Container: React.FunctionComponent<ContainerProps> = (
 ) => {
   return (
     <div
-      css={{
-        width: "100%",
-        maxWidth: "1200px",
-        boxSizing: "border-box",
-        margin: "0 auto",
-        padding: `0 ${theme.spaces.md}`,
-        [theme.breakpoints.lg]: {
-          padding: `0 ${theme.spaces.lg}`
-        }
-      }}
+      css={[
+        {
+          width: "100%",
+          maxWidth: "1200px",
+          boxSizing: "border-box",
+          margin: "0 auto"
+        },
+        responsiveContainerPadding
+      ]}
       {...props}
     />
   );
