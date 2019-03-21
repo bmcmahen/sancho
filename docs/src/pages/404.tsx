e21281d1f22c847c85dfb7728e790dcb26de863e
bmcmahen <ben.mcmahen@gmail.com>
2019-03-21 00:07:04 -0700

    fix 404 docs page

--- docs/src/pages/404.tsx
+++ docs/src/pages/404.tsx
@@ -1,16 +1,15 @@
 import * as React from "react"
-import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import { Text, theme, Link } from "../../../src"
 
 const NotFoundPage = () => (
-  <Layout>
+  <div>
     <SEO title="404: Not found" />
     <Text css={{ display: "block", padding: theme.spaces.lg }}>
       We couldn't find this page. Why not check out the{" "}
       <Link href="/">homepage.</Link>
     </Text>
-  </Layout>
+  </div>
 )
 
 export default NotFoundPage
