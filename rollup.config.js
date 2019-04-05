17f521b1d94550a9d304e30209cf49d3d438129f
bmcmahen <ben.mcmahen@gmail.com>
2019-04-05 13:31:54 -0700

    better builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,32 @@
 import resolve from "rollup-plugin-node-resolve";
 import filesize from "rollup-plugin-filesize";
-import pkg from "./package.json";
 import commonjs from "rollup-plugin-commonjs";
 import cleanup from "rollup-plugin-cleanup";
 import typescript from "rollup-plugin-typescript2";
 import { uglify } from "rollup-plugin-uglify";
+import json from "rollup-plugin-json";
 
-const input = "src/index.tsx";
+const input = "cjs/index.js";
+
+const commonjsOptions = {
+  ignoreGlobal: true,
+  include: /node_modules/,
+  namedExports: {
+    "../../node_modules/prop-types/index.js": [
+      "elementType",
+      "bool",
+      "func",
+      "object",
+      "oneOfType",
+      "element"
+    ]
+  }
+};
 
 const plugins = [
   resolve(),
-  commonjs(),
-  typescript({
-    typescript: require("typescript")
-  }),
+  commonjs(commonjsOptions),
+  json(),
   cleanup(),
   uglify(),
   filesize()
