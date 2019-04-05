7c4c78492383e4b59d2e91e89cb586287c24fbac
bmcmahen <ben.mcmahen@gmail.com>
2019-04-04 21:03:35 -0700

    properly include prop-types in dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,17 @@
 import resolve from "rollup-plugin-node-resolve";
 import filesize from "rollup-plugin-filesize";
-import sourceMaps from "rollup-plugin-sourcemaps";
 import pkg from "./package.json";
-import commonjs from "rollup-plugin-commonjs";
 import cleanup from "rollup-plugin-cleanup";
 import typescript from "rollup-plugin-typescript2";
 
-const input = "src/index.ts";
+const input = "src/index.tsx";
 
 const plugins = [
   resolve(),
   typescript({
     typescript: require("typescript")
   }),
-  commonjs(),
   cleanup(),
-  sourceMaps(),
   filesize()
 ];
 
