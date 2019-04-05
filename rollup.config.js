d27a8d71f71227a596d630e2815642bda93b631b
bmcmahen <ben.mcmahen@gmail.com>
2019-04-05 13:11:51 -0700

    add treeshaking compatible build

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,39 @@
 import resolve from "rollup-plugin-node-resolve";
 import filesize from "rollup-plugin-filesize";
 import pkg from "./package.json";
+import commonjs from "rollup-plugin-commonjs";
 import cleanup from "rollup-plugin-cleanup";
 import typescript from "rollup-plugin-typescript2";
+import { uglify } from "rollup-plugin-uglify";
 
 const input = "src/index.tsx";
 
 const plugins = [
   resolve(),
+  commonjs(),
   typescript({
     typescript: require("typescript")
   }),
   cleanup(),
+  uglify(),
   filesize()
 ];
+
+const globals = {
+  react: "React",
+  "react-doc": "ReactDOM"
+};
 
 export default [
   {
     input,
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {})
-    ],
-    output: [
-      {
-        file: pkg.module,
-        format: "esm",
-        sourcemap: true
-      },
-      {
-        file: pkg.main,
-        format: "cjs",
-        sourcemap: true
-      }
-    ],
+    output: {
+      file: "umd/sancho-ui.production.min.js",
+      format: "umd",
+      name: "Sancho",
+      globals
+    },
+    external: Object.keys(globals),
     plugins
   }
 ];
