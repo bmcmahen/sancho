dbb4395dbacca3c3c5d5fdd72e78a39e0fb4892d
bmcmahen <ben.mcmahen@gmail.com>
2019-04-06 21:59:48 -0700

    revert icon sizes

--- src/Theme/index.ts
+++ src/Theme/index.ts
@@ -67,9 +67,9 @@ const fontWeights = {
 const iconSizes = {
   xs: "0.75rem",
   sm: "1rem",
-  md: "1.5rem",
-  lg: "1.8rem",
-  xl: "2.25rem"
+  md: "1.25rem",
+  lg: "1.5rem",
+  xl: "2rem"
 };
 
 export type ThemeColors = typeof defaultColors.colors;
