da3deec0a68067f01eb674c4ef2993c393ca5966
bmcmahen <ben.mcmahen@gmail.com>
2019-04-18 01:20:22 -0700

    mostly functional side-menu

--- src/GestureSheet.tsx
+++ src/GestureSheet.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { useTransition, animated, useSpring } from "react-spring";
 import { useFocusElement } from "./Hooks/focus";
 import { Overlay } from "./Overlay";
+import { Portal } from "./Portal";
 import { useGesture } from "react-with-gesture";
 import PropTypes from "prop-types";
 import { RemoveScroll } from "react-remove-scroll";
@@ -49,16 +50,25 @@ function getTransitionForPosition(position: SheetPositions) {
 }
 
 const positions = (theme: Theme) => ({
-  left: css({
-    top: 0,
-    left: 0,
-    bottom: 0,
-    width: "auto",
-    maxWidth: "100vw",
-    [theme.mediaQueries.md]: {
-      maxWidth: "400px"
+  left: css`
+    top: 0;
+    left: 0;
+    bottom: 0;
+    width: auto;
+    max-width: 100vw;
+    ${theme.mediaQueries.md} {
+      max-width: 400px;
     }
-  }),
+    &::before {
+      content: "";
+      position: fixed;
+      width: 100vw;
+      top: 0;
+      background: ${theme.colors.background.layer};
+      bottom: 0;
+      transform: translateX(-100%);
+    }
+  `,
   right: css({
     top: 0,
     right: 0,
@@ -145,44 +155,141 @@ export const GestureSheet: React.FunctionComponent<SheetProps> = ({
   ...props
 }) => {
   const theme = useTheme();
+  const [click, setClick] = React.useState();
+  const [mounted, setMounted] = React.useState();
   const { ref, bounds } = useMeasure();
   const positionsStyle = React.useMemo(() => positions(theme), [theme]);
 
-  function getFinalPosition(dx: number, width: number, down: boolean, gone: boolean) {
-
+  function getFinalPosition(
+    dx: number,
+    width: number,
+    down: boolean,
+    isOpen: boolean
+  ) {
     if (down) {
-      return dx 
+      return dx;
     }
 
-    if (dx < 0 || gone) {
-      if (Math.abs(dx) > (width / 2)) {
-        console.log('on request close')
-        onRequestClose()
+    if (dx < 0 || !isOpen) {
+      if (Math.abs(dx) > width / 2) {
+        onRequestClose();
         return dx;
       }
     }
 
-    return 0
+    return 0;
   }
 
-  const [{ x }, setSpring] = useSpring(() => ({ x: bounds.width * -1 }));
+  const [{ x }, setSpring] = useSpring(() => {
+    return {
+      x: isOpen ? 0 : -400
+    };
+  });
 
-  const bind = useGesture(({ down, delta, args }) => {
+  const [{ opacity }, setOpacity] = useSpring(() => {
+    return {
+      opacity: isOpen ? 1 : 0
+    };
+  });
+
+  function getOpacity(dx: number, width: number, isOpen: boolean) {
+    if (!isOpen) {
+      return 0;
+    }
+
+    if (dx < 0) {
+      const percent = Math.abs(dx) / width;
+      return 1 - percent;
+    }
+
+    return 1;
+  }
+
+  const bind = useGesture(({ event, down, delta, args }) => {
     const [dx] = delta;
-    const { width } = args[0]
-    const isOpen = args[1]
-    const x = getFinalPosition(dx, width, down, isOpen)
+    const { width } = args[0];
+    const isOpen = args[1];
+    const x = getFinalPosition(dx, width, down, isOpen);
     setSpring({ x, immediate: down });
+    setOpacity({ immediate: down, opacity: getOpacity(dx, width, isOpen) });
   });
 
   React.useEffect(() => {
-    setSpring({ x: isOpen ? 0 : (bounds.width * -1) })
-  }, [bounds, isOpen])
+    setSpring({ x: isOpen ? 0 : bounds.width * -1 });
+    setOpacity({ opacity: isOpen ? 1 : 0 });
+  }, [bounds, isOpen]);
+
+  function taper(x: number) {
+    if (x <= 0) {
+      return x;
+    }
+
+    return x * 0.4;
+  }
+
+  // 4. support all 4 positions
+
+  // 5. actually unmount when finished?
+  // keep a local close state, and monitor animation to finish.
+
+  function onMouseDown() {
+    setClick(true);
+  }
+
+  function onMouseMove() {
+    setClick(false);
+  }
+
+  function onMouseUp() {
+    if (click) {
+      onRequestClose();
+    }
+  }
 
   return (
-    <React.Fragment>
+    <div
+      aria-hidden={!isOpen}
+      {...bind(bounds, isOpen)}
+      onKeyDown={(e: React.KeyboardEvent) => {
+        if (e.key === "Escape") {
+          e.stopPropagation();
+          onRequestClose();
+        }
+      }}
+      css={{
+        bottom: 0,
+        left: 0,
+        overflow: "auto",
+        width: "100vw",
+        height: "100vh",
+        pointerEvents: isOpen ? "auto" : "none",
+        zIndex: theme.zIndices.overlay,
+        position: "fixed",
+        content: "''",
+        right: 0,
+        top: 0,
+        WebkitTapHighlightColor: "transparent"
+      }}
+    >
       <animated.div
-        {...bind(bounds, isOpen)}
+        style={{ opacity }}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+        onTouchStart={onMouseDown}
+        onTouchMove={onMouseMove}
+        onTouchEnd={onMouseUp}
+        css={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          pointerEvents: isOpen ? "auto" : "none",
+          right: 0,
+          bottom: 0,
+          background: theme.colors.background.overlay
+        }}
+      />
+      <animated.div
         tabIndex={-1}
         ref={ref}
         className="Sheet"
@@ -191,7 +298,7 @@ export const GestureSheet: React.FunctionComponent<SheetProps> = ({
         }}
         style={{
           transform: x.interpolate(x => {
-            return `translateX(${x}px)`;
+            return `translateX(${taper(x)}px)`;
           })
         }}
         css={[
@@ -210,7 +317,6 @@ export const GestureSheet: React.FunctionComponent<SheetProps> = ({
             <div
               className="Sheet__container"
               css={{
-                boxShadow: theme.shadows.md,
                 background: theme.colors.background.layer,
                 height: "100%"
               }}
@@ -220,7 +326,7 @@ export const GestureSheet: React.FunctionComponent<SheetProps> = ({
           </RemoveScroll>
         </RequestCloseContext.Provider>
       </animated.div>
-    </React.Fragment>
+    </div>
   );
 };
 
