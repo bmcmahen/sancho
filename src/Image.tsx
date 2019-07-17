05a3d2c635a0dd8e8ff09442b819ef66c9bb211e
bmcmahen <ben.mcmahen@gmail.com>
2019-07-17 16:14:11 -0700

    fix bug related to scrolling

--- src/Image.tsx
+++ src/Image.tsx
@@ -1,9 +1,10 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
 import * as React from "react";
-import { useSpring, animated } from "react-spring";
-import { number } from "prop-types";
+import { useSpring, animated, config } from "react-spring";
 import { usePrevious } from "./Collapse";
+import { useGestureResponder, StateType } from "react-gesture-responder";
+import { useTheme } from "./Theme/Providers";
 
 interface PositionType {
   x: number;
@@ -16,7 +17,7 @@ export interface ImageProps extends React.HTMLAttributes<any> {
   fadeIn?: boolean;
   zoomed?: boolean;
   onClick: () => void;
-  onRequestClose?: boolean;
+  onRequestClose?: () => void;
   src: string;
 }
 
@@ -41,10 +42,8 @@ const initialDimensions = {
 
 const initialTransform = "translateX(0px) translateY(0px) scale(1)";
 
-const ImageContext = React.createContext({
-  zoomed: false,
-  dimensions: initialDimensions
-});
+const getScale = linearConversion([0, 500], [1, 0.4]);
+const scaleClamp = clamp(0.4, 1);
 
 export const Image: React.FunctionComponent<ImageProps> = ({
   fadeIn,
@@ -52,15 +51,13 @@ export const Image: React.FunctionComponent<ImageProps> = ({
   onRequestClose,
   ...other
 }) => {
+  const theme = useTheme();
   const ref = React.useRef<HTMLImageElement>(null);
   const prevZoom = usePrevious(zoomed);
   const cloneRef = React.useRef<any>(null);
 
-  const [cloneSize, setCloneSize] = React.useState({
-    width: 0,
-    height: 0,
-    loaded: false
-  });
+  const [cloneLoaded, setCloneLoaded] = React.useState(false);
+  const prevCloneLoaded = usePrevious(cloneLoaded);
 
   const [thumbProps, setThumbProps] = useSpring(() => ({
     opacity: 1,
@@ -78,16 +75,57 @@ export const Image: React.FunctionComponent<ImageProps> = ({
     top: 0,
     width: 0,
     height: 0,
-    immediate: true
+    immediate: true,
+    config: config.stiff
   }));
 
-  const prevCloneSize = usePrevious(cloneSize);
+  function onMove({ delta }: StateType) {
+    const scale = scaleClamp(getScale(Math.abs(delta[1])));
+
+    set({
+      transform: `translateX(${delta[0] * 0.8}px) translateY(${
+        delta[1]
+      }px) scale(${scale})`,
+      immediate: true
+    });
+
+    setOverlay({ opacity: scale, immediate: true });
+  }
+
+  function onEnd({ delta }: StateType) {
+    if (Math.abs(delta[1]) > 20 && onRequestClose) {
+      onRequestClose();
+    } else {
+      // reset
+      set({ transform: initialTransform, immediate: false });
+      setOverlay({ opacity: 1, immediate: false });
+    }
+  }
+
+  const { bind } = useGestureResponder({
+    onStartShouldSet: () => false,
+    onMoveShouldSet: ({ initialDirection, delta }) => {
+      if (!zoomed) {
+        return false;
+      }
+
+      return true;
+    },
+    onMove: onMove,
+    onRelease: onEnd,
+    onTerminate: onEnd
+  });
 
   const generatePositions = React.useCallback(
-    (immediate?: boolean) => {
+    (immediate?: boolean = false) => {
       // any time this prop changes, we update our position
-      if (ref.current && cloneSize.loaded) {
+      if (ref.current && cloneLoaded) {
         const rect = ref.current.getBoundingClientRect();
+
+        const cloneSize = {
+          width: cloneRef.current!.naturalWidth,
+          height: cloneRef.current!.naturalHeight
+        };
 
         const thumbDimensions = {
           x: rect.left,
@@ -106,51 +144,63 @@ export const Image: React.FunctionComponent<ImageProps> = ({
           clonedDimensions
         );
 
+        console.log(thumbDimensions, initialSize);
+
         const zoomingIn = !prevZoom && zoomed;
         const zoomingOut = prevZoom && !zoomed;
 
         if (zoomingIn) {
+          console.log("zooming in");
           setThumbProps({ opacity: 0, immediate: true });
-          set({ opacity: 1, immediate: true });
+          set({
+            opacity: 1,
+            immediate: true,
+            transform: `translateX(${initialSize.translateX}px) translateY(${
+              initialSize.translateY
+            }px) scale(${initialSize.scale})`,
+            left: clonedDimensions.x,
+            top: clonedDimensions.y,
+            width: clonedDimensions.w,
+            height: clonedDimensions.h,
+            onRest: null
+          });
+
+          set({
+            transform: initialTransform,
+            immediate: false
+          });
+        } else if (zoomingOut) {
+          console.log("zooming out");
+          set({
+            transform: `translateX(${initialSize.translateX}px) translateY(${
+              initialSize.translateY
+            }px) scale(${initialSize.scale})`,
+            immediate: false,
+            onRest: () => {
+              setThumbProps({ opacity: 1, immediate: true });
+              set({ opacity: 0, immediate: true });
+            }
+          });
         }
 
         setOverlay({ opacity: zoomed ? 1 : 0 });
-
-        set({
-          transform: zoomed
-            ? initialTransform
-            : `translateX(${initialSize.translateX}px) translateY(${
-                initialSize.translateY
-              }px) scale(${initialSize.scale})`,
-          left: clonedDimensions.x,
-          top: clonedDimensions.y,
-          width: clonedDimensions.w,
-          height: clonedDimensions.h,
-          immediate: immediate || (prevCloneSize && !prevCloneSize.loaded),
-          onRest: zoomingOut
-            ? () => {
-                setThumbProps({ opacity: 1, immediate: true });
-                set({ opacity: 0, immediate: true });
-              }
-            : null
-        });
       }
     },
-    [zoomed, cloneSize, ref, prevCloneSize, prevZoom]
+    [zoomed, cloneLoaded, ref, cloneRef, prevCloneLoaded, prevZoom]
   );
 
   const onResize = React.useCallback(() => {
     generatePositions(true);
-  }, [zoomed, cloneSize, ref, prevCloneSize, prevZoom]);
+  }, [zoomed, cloneLoaded, ref, prevCloneLoaded, prevZoom]);
 
   // update our various positions
   React.useEffect(() => {
     generatePositions();
-    window.addEventListener("resize", onResize);
+    if (zoomed) window.addEventListener("resize", onResize);
     return () => {
       window.removeEventListener("resize", onResize);
     };
-  }, [zoomed, cloneSize, ref, prevCloneSize, prevZoom]);
+  }, [zoomed, cloneLoaded, ref, prevCloneLoaded, prevZoom]);
 
   return (
     <React.Fragment>
@@ -162,10 +212,12 @@ export const Image: React.FunctionComponent<ImageProps> = ({
         {...other}
       />
       <animated.div
+        {...bind}
         style={{
           opacity: overlay.opacity
         }}
         aria-hidden={!zoomed}
+        onClick={onRequestClose}
         css={{
           pointerEvents: zoomed ? "auto" : "none",
           position: "fixed",
@@ -174,16 +226,13 @@ export const Image: React.FunctionComponent<ImageProps> = ({
           bottom: 0,
           right: 0,
           zIndex: 90,
-          background: "rgba(255,255,255,0.8)"
+          background: theme.colors.background.default
         }}
       />
       <animated.img
+        css={{ pointerEvents: "none" }}
         onLoad={() => {
-          setCloneSize({
-            width: cloneRef.current!.naturalWidth,
-            height: cloneRef.current!.naturalHeight,
-            loaded: true
-          });
+          setCloneLoaded(true);
         }}
         style={{
           zIndex: 100,
@@ -271,4 +320,31 @@ function getViewport() {
   }
 
   return { width: 0, height: 0 };
+}
+
+/**
+ * Create a basic linear conversion fn
+ */
+
+function linearConversion(a: [number, number], b: [number, number]) {
+  const o = a[1] - a[0];
+  const n = b[1] - b[0];
+
+  return function(x: number) {
+    return ((x - a[0]) * n) / o + b[0];
+  };
+}
+
+/**
+ * Create a clamp
+ * @param max
+ * @param min
+ */
+
+function clamp(min: number, max: number) {
+  return function(x: number) {
+    if (x > max) return max;
+    if (x < min) return min;
+    return x;
+  };
 }
--- src/stories/Image.stories.tsx
+++ src/stories/Image.stories.tsx
@@ -4,21 +4,73 @@ import { storiesOf } from "@storybook/react";
 import defaultTheme from "../Theme";
 import { Image } from "../Image";
 import * as React from "react";
+import { Container } from "../Container";
+import { Text } from "../Text";
 
 const src =
-  "https://images.unsplash.com/photo-1563377176922-062e6ae09ceb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80";
+  "https://images.unsplash.com/photo-1563285851-d81750ac22fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80";
 
 function ZoomedExample() {
   const [zoom, setZoom] = React.useState(false);
 
   return (
-    <div>
-      <Image
-        css={{ width: "300px", height: "auto" }}
-        src={src}
-        zoomed={zoom}
-        onClick={() => setZoom(!zoom)}
-      />
+    <div css={{ padding: "2rem" }}>
+      <Container css={{ maxWidth: "650px" }}>
+        <Text variant="h2">Nulla magna commodo deserunt ad.</Text>
+        <Text variant="lead">
+          Exercitation exercitation officia ea eu enim laborum eiusmod fugiat
+          aute Lorem reprehenderit nulla officia.
+        </Text>
+        <Text css={{ marginTop: "1rem" }} variant="paragraph">
+          Ipsum elit duis minim sint adipisicing reprehenderit aliqua ut
+          excepteur velit adipisicing. Ea officia do ullamco commodo esse
+          consectetur elit enim elit elit et. Aute aliqua quis minim tempor id
+          commodo magna.
+        </Text>
+
+        <div css={{ marginRight: "1rem", marginLeft: 0, float: "left" }}>
+          <Image
+            css={{
+              width: "300px",
+              height: "auto"
+            }}
+            src={src}
+            zoomed={zoom}
+            onClick={() => setZoom(!zoom)}
+            onRequestClose={() => setZoom(false)}
+          />
+        </div>
+        <Text variant="paragraph">
+          Ipsum elit duis minim sint adipisicing reprehenderit aliqua ut
+          excepteur velit adipisicing. Ea officia do ullamco commodo esse
+          consectetur elit enim elit elit et. Aute aliqua quis minim tempor id
+          commodo magna.
+        </Text>
+        <Text variant="paragraph">
+          Lorem aliqua laboris excepteur ut proident velit et pariatur commodo.
+          Consectetur minim aliquip tempor ullamco eu. Sint ullamco nulla et
+          magna officia ullamco proident sunt in dolore do ea nostrud nostrud.
+          Velit qui et aute voluptate. Sint aute Lorem minim ea dolore minim
+          aute commodo ex.
+        </Text>
+        <Text variant="paragraph">
+          Ipsum elit duis minim sint adipisicing reprehenderit aliqua ut
+          excepteur velit adipisicing. Ea officia do ullamco commodo esse
+          consectetur elit enim elit elit et. Aute aliqua quis minim tempor id
+          commodo magna. Minim laborum eu veniam exercitation fugiat veniam
+          voluptate dolore officia labore nostrud. Duis magna mollit proident
+          elit ullamco. Laborum ea ipsum pariatur elit id quis do ut Lorem
+          eiusmod. Consectetur amet commodo occaecat adipisicing ex est ad
+          deserunt minim deserunt culpa adipisicing nulla.
+        </Text>
+        <Text variant="paragraph">
+          Lorem aliqua laboris excepteur ut proident velit et pariatur commodo.
+          Consectetur minim aliquip tempor ullamco eu. Sint ullamco nulla et
+          magna officia ullamco proident sunt in dolore do ea nostrud nostrud.
+          Velit qui et aute voluptate. Sint aute Lorem minim ea dolore minim
+          aute commodo ex.
+        </Text>
+      </Container>
     </div>
   );
 }
